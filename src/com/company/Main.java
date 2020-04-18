a172f326d7b2dd989f47ee58e1feab3817307ec7
Hamtaro30214 <marcin-ros98@wp.pl>
2020-04-18 18:45:14 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal dog = new Animal("Dog");
+        dog.name = "Szarik";
+
+        System.out.println("Hi, I'm " + dog.name);
+
+        dog.feed();
+
+        Human me = new Human();
+        me.firstName = "Marcin";
+        me.lastName = "Rosicki";
+        me.pet = dog;
+
+        me.pet.feed();
+        System.out.println(me.pet.species);
+
+        me.pet = new Animal("Lion");
+        me.pet.name = "WielkiKot2 ";
+
+        me.pet.feed();
+
+        System.out.println(me.pet.species);
+    }
+}
